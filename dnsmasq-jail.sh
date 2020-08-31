7f4da737a3cdb539ec21a2bb450d879f8bec0305
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2020-08-31 15:09:14 +0800

    Create dnsmasq-jail.sh

---
+++ dnsmasq-jail.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+# Build an iocage jail under FreeNAS 11.3-12.0 using the current release of DNSMasq
+# git clone https://github.com/basilhendroff/freenas-iocage-dnsmasq
+
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
+
+#####
+#
+# General configuration
+#
+#####
+
+# Initialize defaults
+JAIL_IP=""
+JAIL_INTERFACES=""
+DEFAULT_GW_IP=""
+INTERFACE="vnet0"
+VNET="on"
+POOL_PATH=""
+JAIL_NAME="dnsmasq"
+CONFIG_NAME="dnsmasq-config"
+CONFIG_PATH=""
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+
+# Check for dnsmasq-config and set configuration
+if ! [ -e "${SCRIPTPATH}"/"${CONFIG_NAME}" ]; then
+  echo "${SCRIPTPATH}/${CONFIG_NAME} must exist."
+  exit 1
+fi
+. "${SCRIPTPATH}"/"${CONFIG_NAME}"
+INCLUDES_PATH="${SCRIPTPATH}"/includes
+
+JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
+RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p[0-9]*//")
+
+#####
+#
+# Input/Config Sanity checks
+#
+#####
+
+# Check that necessary variables were set by rslsync-config
+if [ -z "${JAIL_IP}" ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z "${JAIL_INTERFACES}" ]; then
+  echo 'JAIL_INTERFACES not set, defaulting to: vnet0:bridge0'
+  JAIL_INTERFACES="vnet0:bridge0"
+fi
+if [ -z "${DEFAULT_GW_IP}" ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z "${POOL_PATH}" ]; then
+  POOL_PATH="/mnt/$(iocage get -p)"
+  echo 'POOL_PATH defaulting to '$POOL_PATH
+fi
+
+# If CONFIG_PATH wasn't set in dnsmasq-config, set it.
+if [ -z "${CONFIG_PATH}" ]; then
+  CONFIG_PATH="${POOL_PATH}"/apps/dnsmasq/
+fi
+
+if [ "${CONFIG_PATH}" = "${POOL_PATH}" ]
+then
+  echo "CONFIG_PATH must be different from POOL_PATH!"
+  exit 1
+fi
+
+# Extract IP and netmask, sanity check netmask
+IP=$(echo ${JAIL_IP} | cut -f1 -d/)
+NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
+if [ "${NETMASK}" = "${IP}" ]
+then
+  NETMASK="24"
+fi
+if [ "${NETMASK}" -lt 8 ] || [ "${NETMASK}" -gt 30 ]
+then
+  NETMASK="24"
+fi
+
+#####
+#
+# Jail Creation
+#
+#####
+
+# List packages to be auto-installed after jail creation
+cat <<__EOF__ >/tmp/pkg.json
+	{
+  "pkgs":[
+  "nano","bash","dnsmasq"
+  ]
+}
+__EOF__
+
+# Create the jail and install previously listed packages
+if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" interfaces="${JAIL_INTERFACES}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+then
+	echo "Failed to create jail"
+	exit 1
+fi
+rm /tmp/pkg.json
+
+#####
+#
+# Directory Creation and Mounting
+#
+#####
+
+mkdir -p "${CONFIG_PATH}"
+
+iocage exec "${JAIL_NAME}" mkdir -p /config
+iocage fstab -a "${JAIL_NAME}" "${CONFIG_PATH}" /config nullfs rw 0 0
+
+#####
+#
+# DNSMasq Setup
+#
+#####
+
+iocage exec "${JAIL_NAME}" cp /usr/local/etc/dnsmasq.conf.sample /config
+
+iocage exec "${JAIL_NAME}" sysrc dnsmasq_enable="YES"
+iocage exec "${JAIL_NAME}" sysrc dnsmasq_conf="/config/dnsmasq.conf"
+
+iocage restart "${JAIL_NAME}"
